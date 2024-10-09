1bfa36b4f2eb615c450d235a82912f1ee435a945
sanie <130692629+yakubova6@users.noreply.github.com>
2024-10-10 00:05:18 +0300

    Update main.cpp

--- contest_01/19/main.cpp
+++ contest_01/19/main.cpp
@@ -1,1 +1,27 @@
-﻿
+#include <iostream>
+#include <set>
+#include <string>
+#include <map>
+
+int main()
+{
+  std::string word;
+	std::map<std::string, int> word_count;
+	while (std::cin >> word)
+	{
+		word_count[word]++;
+	}
+	std::set <std::string> result;
+	for (auto& entry : word_count)
+	{
+		if (entry.second > 1)
+		{
+			result.insert(entry.first);
+		}
+	}
+	for (const std::string& w : result)
+	{
+		std::cout << w << " ";
+	}
+	return 0;
+}
